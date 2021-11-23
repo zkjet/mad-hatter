57e8d0fb12ddb68c76669e75d5db0c7b3c0a9895
jonvaljonathan.eth <jonathan42white@gmail.com>
2021-11-22 18:54:07 -0600

    add /roll

--- src/app/service/coordinape/HandleNoEpoc.ts
+++
@@ -1,6 +0,0 @@
-import { GuildMember } from 'discord.js';
-import { CommandContext } from 'slash-create';
-
-export const handleNoEpochs = async (guidlMember:GuildMember, ctx: CommandContext, coordinapeUser): Promise<any> => {
-	return await ctx.send(`Sorry ${coordinapeUser} ser, there are currently no active epochs.`);
-};
--- src/app/service/coordinape/Help.ts
+++
@@ -1,6 +0,0 @@
-import { CommandContext } from 'slash-create';
-
-// /help - link to documentation
-export const Help = async (ctx: CommandContext): Promise<any> => {
-	return await ctx.send('Help is on the way!');
-};
