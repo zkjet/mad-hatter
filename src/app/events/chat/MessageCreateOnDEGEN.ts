a4c295fceb0b6381dff2791a122abb1b42e8728e
Brian <patinobrian@gmail.com>
2021-11-23 21:24:02 -0500

    add catch blocks

--- src/app/events/chat/MessageCreateOnDEGEN.ts
+++ src/app/events/chat/MessageCreateOnDEGEN.ts
@@ -1,6 +1,6 @@
 import { Message } from 'discord.js';
 import degenPhrases from '../../service/constants/degenPhrases';
-import { LogUtils } from '../../utils/Log';
+import Log, { LogUtils } from '../../utils/Log';
 import constants from '../../service/constants/constants';
 
 const MessageCreateOnDEGEN = async (message: Message): Promise<void> => {
@@ -9,25 +9,25 @@ const MessageCreateOnDEGEN = async (message: Message): Promise<void> => {
 		
 		// POAP
 		if (content.match(/POAP/gi)) {
-			message.channel.send({
+			await message.channel.send({
 				content: `${degenPhrases.poap[Math.floor(Math.random() * degenPhrases.poap.length)]}`,
-			});
+			}).catch(Log.error);
 			return;
 		}
 	
 		// APP name
 		if (content.match(/Mad Hatter/gi) || message.mentions.has(constants.DISCORD_BOT_USER_ID)) {
-			message.channel.send({
+			await message.channel.send({
 				content: `${degenPhrases.app[Math.floor(Math.random() * degenPhrases.app.length)]}`,
-			});
+			}).catch(Log.error);
 			return;
 		}
 		
 		// gm
 		if (content.match(/gm/g)) {
-			message.channel.send({
+			await message.channel.send({
 				content: 'gm',
-			});
+			}).catch(Log.error);
 			return;
 		}
 	} catch (e) {
