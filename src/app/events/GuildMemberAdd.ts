5d0f71e3eb33762f9f2dee17c67c5cd085a07f2a
Brian <patinobrian@gmail.com>
2021-11-25 15:08:17 -0500

    wip

--- src/app/events/GuildMemberAdd.ts
+++ src/app/events/GuildMemberAdd.ts
@@ -1,7 +1,7 @@
 import { GuildMember } from 'discord.js';
 import { DiscordEvent } from '../types/discord/DiscordEvent';
 import ServiceUtils from '../utils/ServiceUtils';
-import { LogUtils } from '../utils/Log';
+import Log, { LogUtils } from '../utils/Log';
 import { sendFqMessage } from '../service/first-quest/LaunchFirstQuest';
 import client from '../app';
 import { Captcha } from 'discord.js-captcha';
@@ -21,59 +21,52 @@ export default class implements DiscordEvent {
 			
 			if (await ServiceUtils.runUsernameSpamFilter(member)) {
 				return;
-			} else {
-				const captchaOptions = {
-					guildID: member.guild.id,
-					roleID: fqConstants.FIRST_QUEST_ROLES.verified,
-					channelID: channelIds.captchaVerification,
-					kickOnFailure: false,
-					attempts: 1,
-					timeout: 180000,
-					showAttemptCount: true,
-				};
-				
-				const captcha1 = new Captcha(client, captchaOptions);
-				captcha1.present(member);
-				handleCaptchaSuccess(member, captcha1);
-				
-				captcha1.on('failure', () => {
-					const captcha2 = new Captcha(client, captchaOptions);
-					captcha2.present(member);
-					handleCaptchaSuccess(member, captcha2);
-					
-					captcha2.on('failure', () => {
-						captchaOptions.kickOnFailure = true;
-						const captcha3 = new Captcha(client, captchaOptions);
-						captcha3.present(member);
-						handleCaptchaSuccess(member, captcha3);
-						
-						captcha3.on('timeout', () => {
-							member.kick('captcha timeout');
-							
-						});
-					});
-					
-					captcha2.on('timeout', () => {
-						member.kick('captcha timeout');
-					});
-				});
-				
-				captcha1.on('timeout', () => {
-					member.kick('captcha timeout');
-				});
-				
 			}
+			
+			const captchaOptions = {
+				guildID: member.guild.id,
+				roleID: fqConstants.FIRST_QUEST_ROLES.verified,
+				channelID: channelIds.captchaVerification,
+				kickOnFailure: false,
+				attempts: 1,
+				timeout: 180000,
+				showAttemptCount: true,
+				caseSensitive: true,
+			};
+			
+			let attempts = 0;
+			while (attempts < 3) {
+				const captcha = new Captcha(client, captchaOptions);
+				captcha.present(member);
+				await waitForCaptcha(member, captcha).catch();
+				attempts++;
+			}
+			
 		} catch (e) {
-			LogUtils.logError('failed to process event guildMemberAdd', e);
+			Log.error('failed to process event guildMemberAdd');
 		}
 	}
 }
 
-const handleCaptchaSuccess = (member: GuildMember, captcha: Captcha) => {
-	captcha.on('success', async () => {
-		await member.roles.add(fqConstants.FIRST_QUEST_ROLES.verified);
-		await sendFqMessage('undefined', member).catch(e => {
-			LogUtils.logError('First attempt to launch first-quest failed: ', e);
+const waitForCaptcha = async (member: GuildMember, captcha: Captcha): Promise<void> => {
+	return new Promise<void>((resolve, reject) => {
+		captcha.on('success', async (data) => {
+			console.log(data);
+			await member.roles.add(fqConstants.FIRST_QUEST_ROLES.verified);
+			// await sendFqMessage('undefined', member).catch(e => {
+			// 	LogUtils.logError('First attempt to launch first-quest failed: ', e);
+			// });
+			resolve();
+		});
+		
+		captcha.on('failure', data => {
+			console.log(data);
+			reject();
+		});
+		
+		captcha.on('timeout', () => {
+			member.kick('captcha timeout');
+			reject();
 		});
 	});
 };
