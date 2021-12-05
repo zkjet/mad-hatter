439483e64d960a98ba168ddd4fb8dcdbe0c92fd1
Austin Mitchem <austinmitchem@outlook.com>
2021-12-05 16:29:51 -0600

    fix linting

--- src/app/utils/Tracer.ts
+++ src/app/utils/Tracer.ts
@@ -1,37 +1,42 @@
 import process from 'process';
-import { Metadata, credentials } from "@grpc/grpc-js";
-
+import { Metadata, credentials } from '@grpc/grpc-js';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import Log from './Log';
 
-const metadata = new Metadata()
+new Log();
+
+const metadata = new Metadata();
 metadata.set('x-honeycomb-team', process.env.HONEYCOMB_APIKEY);
 metadata.set('x-honeycomb-dataset', process.env.HONEYCOMB_DATASET);
 const traceExporter = new OTLPTraceExporter({
-  url: 'grpc://api.honeycomb.io:443/',
-  credentials: credentials.createSsl(),
-  metadata
+	url: 'grpc://api.honeycomb.io:443/',
+	credentials: credentials.createSsl(),
+	metadata,
 });
 
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: process.env.HONEYCOMB_DATASET,
-  }),
-  traceExporter,
-  // @ts-ignore
-  instrumentations: [getNodeAutoInstrumentations()] 
+	resource: new Resource({
+		[SemanticResourceAttributes.SERVICE_NAME]: process.env.HONEYCOMB_DATASET,
+	}),
+	traceExporter,
+});
+
+registerInstrumentations({
+	instrumentations: [getNodeAutoInstrumentations()],
 });
 
 sdk.start()
-  .then(() => console.log('Tracing initialized'))
-  .catch((error) => console.log('Error initializing tracing', error));
+	.then(() => Log.log('Tracing initialized'))
+	.catch((error) => Log.error('Error initializing tracing', error));
 
 process.on('SIGTERM', () => {
-  sdk.shutdown()
-    .then(() => console.log('Tracing terminated'))
-    .catch((error) => console.log('Error terminating tracing', error))
-    .finally(() => process.exit(0));
+	sdk.shutdown()
+		.then(() => Log.log('Tracing terminated'))
+		.catch((error) => Log.error('Error terminating tracing', error))
+		.finally(() => process.exit(0));
 });
