0623b84478c96487cdb61a00ffdea9cdaa99afa4
Brian <patinobrian@gmail.com>
2021-12-06 11:17:19 -0500

    revert changes

--- src/app/events/chat/MessageCreateOnDEGEN.ts
+++ src/app/events/chat/MessageCreateOnDEGEN.ts
@@ -1,7 +1,4 @@
-import {
-	Channel,
-	Message,
-} from 'discord.js';
+import { Message } from 'discord.js';
 import degenPhrases from '../../service/constants/degenPhrases';
 import Log, { LogUtils } from '../../utils/Log';
 import constants from '../../service/constants/constants';
@@ -9,7 +6,7 @@ import constants from '../../service/constants/constants';
 const MessageCreateOnDEGEN = async (message: Message): Promise<void> => {
 	try {
 		const content: string = message.content;
-
+		
 		// POAP
 		if (content.match(/POAP/gi)) {
 			await message.channel.sendTyping();
@@ -18,28 +15,16 @@ const MessageCreateOnDEGEN = async (message: Message): Promise<void> => {
 			}).catch(Log.error);
 			return;
 		}
-
+	
 		// APP name
 		if (content.match(/Mad Hatter/gi) || message.mentions.has(constants.DISCORD_BOT_USER_ID)) {
 			await message.channel.sendTyping();
-			const phrase = degenPhrases.app[Math.floor(Math.random() * degenPhrases.app.length)];
-			const degenMessage = await message.channel.send({ content: phrase }).catch(Log.error) as Message;
-
-			if (phrase === 'This is your last chance. After this, there is no turning back. ' +
-				'You take the blue pill - the story ends. You take the red pill - ' +
-				'you stay in Wonderland, and I show you how deep the rabbit hole goes.') {
-
-				try {
-					await degenMessage.react('🔵');
-					await degenMessage.react('🔴');
-				} catch (e) {
-					LogUtils.logError('message reaction failed', e);
-				}
-			}
-
+			await message.channel.send({
+				content: `${degenPhrases.app[Math.floor(Math.random() * degenPhrases.app.length)]}`,
+			}).catch(Log.error);
 			return;
 		}
-
+		
 		// gm
 		if (content.match(/gm/g)) {
 			await message.channel.sendTyping();
--- src/app/service/constants/degenPhrases.ts
+++ src/app/service/constants/degenPhrases.ts
@@ -45,9 +45,6 @@ const degenPhrases = {
 		'👀',
 		'Hello! I\'m so glad you are here! wgmi fren',
 		'Shill me your project 👂',
-		'This is your last chance. After this, there is no turning back. ' +
-		'You take the blue pill - the story ends. You take the red pill - ' +
-		'you stay in Wonderland, and I show you how deep the rabbit hole goes.',
 	],
 };
 
